c5100197211343496a46e478c66c7ddb658efe40
Vishnu Bollam <bollamvishnu13@gmail.com>
2025-02-15 23:49:25 -0500

    Stack updated

--- cdk-starter/lib/cdk-starter-stack.ts
+++ cdk-starter/lib/cdk-starter-stack.ts
@@ -1,14 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
+import { CfnOutput, CfnParameter, Duration } from 'aws-cdk-lib';
 import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
-import { Duration } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-class L3Bucket extends Construct{
-  constructor(scope: Construct, id: string, expiration: number){
-    super(scope,id)
+class L3Bucket extends Construct {
+  constructor(scope: Construct, id: string, expiration: number) {
+    super(scope, id);
 
-    new Bucket (this, 'MyL3Bucket',{
+    new Bucket(this, 'L3Bucket', {
       lifecycleRules: [{
         expiration: Duration.days(expiration)
       }]
@@ -20,7 +19,8 @@ export class CdkStarterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CfnBucket(this, 'MyL1Bucket',{
+    // create an s3 bucket 3 ways:
+    new CfnBucket(this, 'MyL1Bucket', {
       lifecycleConfiguration:{
         rules:[{
           expirationInDays: 1,
@@ -29,12 +29,28 @@ export class CdkStarterStack extends cdk.Stack {
       }
     });
 
-    new Bucket (this, 'MyL2Bucket',{
+    const duration = new CfnParameter(this, 'duration', {
+      default: 6,
+      minValue: 1,
+      maxValue: 10,
+      type: 'Number'
+    })
+
+    const myL2Bucket = new Bucket(this, 'MyL2Bucket', {
       lifecycleRules: [{
-         expiration: Duration.days(2)
+        expiration: Duration.days(duration.valueAsNumber)
       }]
-    } );
+    });
+    
+    new CfnOutput(this, 'MyL2BucketName', {
+      value: myL2Bucket.bucketName
+    })
+
+
 
     new L3Bucket(this, 'MyL3Bucket', 3);
+
+
+
   }
 }
